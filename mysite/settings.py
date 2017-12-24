fe1e94ad4f5f336ae91d65d85322a78560fde7a8
djangotutor <djangotutor@gmail.com>
2017-12-25 06:06:24 +0900

    기본 설정 변경 TIME_ZONE, ALLOWED_HOST 변경, STATIC_ROOT 추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')5)78izvq+)+iit=x^2a&$3%q08q3t)^x%xv5$xd-q(_s7hm+q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
