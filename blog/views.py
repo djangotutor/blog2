0c81a7145cc3b10e7bab2367b3d76071b9de327d
djangotutor <djangotutor@gmail.com>
2017-12-25 09:48:09 +0900

    카테고리 상세 출력용 뷰 작성 generic DetailView를 상속받아서 작성

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Category
 
 class CategoryList(ListView):
 	model = Category
 	context_object_name = 'categorys'
+
+class CategoryDetail(DetailView):
+	model = Category
+	context_object_name = 'category'
