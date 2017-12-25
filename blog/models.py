1d054d364896282c6d31f48243eb24cb849986e5
djangotutor <djangotutor@gmail.com>
2017-12-25 10:26:55 +0900

    Category 모델 클래스에 get_absolute_url() 메서드 추가 generic CreateView, UpdateView 에서 모든 작업이 정상 처리된 후, 리디렉션 주소값으로 사용

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
 	name = models.CharField(max_length=100)
 	tagline = models.TextField()
 	is_open = models.BooleanField(default=True)
 	ordering = models.PositiveSmallIntegerField()
+
+	def get_absolute_url(self):
+		return reverse('category_detail', kwargs={'pk':self.pk})
 
 	def __str__(self):
 		return self.name
