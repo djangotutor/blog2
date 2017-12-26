070c7253c407d015880de232bcd3a3500cf990f1
djangotutor <djangotutor@gmail.com>
2017-12-26 15:40:28 +0900

    Post 모델 작성및 디비생성 관리자 페이지에서 해당 모델을 관리하기 위해 admin.py에 등록

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 class Category(models.Model):
 	name = models.CharField(max_length=100)
@@ -12,3 +13,18 @@ class Category(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Post(models.Model):
+	category = models.ForeignKey('blog.Category')
+	author = models.ForeignKey('auth.User')
+	title = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	published_date = models.DateTimeField(blank=True, null=True)
+
+	def publish(self):
+		self.published_date = timezone.now()
+		self.save()
+
+	def __str__(self):
+		return self.title
