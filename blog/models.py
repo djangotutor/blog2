e4fb734907540f0db9d5a85df29f83336808994f
djangotutor <djangotutor@gmail.com>
2017-12-28 08:58:35 +0900

    댓글을 위한 Comment 모델 작성 모델 작성및 데이터베이스 테이블 생성

--- blog/models.py
+++ blog/models.py
@@ -31,3 +31,20 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Comment(models.Model):
+	post = models.ForeignKey('blog.post', related_name="comments")
+	author = models.CharField(max_length=200)
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
+
+	def approve(self):
+		self.approved_comment = True
+		self.save()
+
+	def get_absolute_url(self):
+		return reverse('post_detail', kwargs={'pk':self.post.pk})
+
+	def __str__(self):
+		return self.text
